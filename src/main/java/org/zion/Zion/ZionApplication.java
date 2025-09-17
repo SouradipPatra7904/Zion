9b72cf312ef5ab84385113b6193ffca9b6f89c4f
Souradip Patra <spectralphoenix.dev@gmail.com>
2025-09-17 13:28:02 +0530

    New Levels, New Devils : CQRS + Event Sourcing with Kafka/Axon project has been scaffolded.

---
+++ src/main/java/org/zion/Zion/ZionApplication.java
@@ -0,0 +1,13 @@
+package org.zion.Zion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zion/Zion/ZionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zion.Zion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
